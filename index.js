96625e8304f4e6344236aa2c7e1dee109a8cf76d
cdROma <mrv24ru@yandex.ru>
2021-10-22 17:59:13 +0300

    fix: port fixed

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3344;
 const rateLimit = require("express-rate-limit");
-const bunyan = require('bunyan');
+const bunyan = require("bunyan");
 
-const log = bunyan.createLogger({name: 'ips', streams: [{path: process.env.LOG_PATH}]});
+const log = bunyan.createLogger({
+  name: "ips",
+  streams: [{ path: process.env.LOG_PATH }],
+});
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    message: {
-        error: {
-            code: 429,
-            message: "Too many requests from your IP. Please wait 15 Minutes",
-        },
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: {
+    error: {
+      code: 429,
+      message: "Too many requests from your IP. Please wait 15 Minutes",
     },
+  },
 });
 
 app.use(limiter);
 
-
-app.get('/', (req, res) => {
-
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    log.info(ip);
-    res.redirect(process.env.URL_TO_REDIRECT)
-})
+app.get("/", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  log.info(ip);
+  res.redirect(process.env.URL_TO_REDIRECT);
+});
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
